f1b8cae1489ba500b701eb09aeb9136aac63220b
Omoshu <omoshulol@gmail.com>
2018-08-08 00:06:36 -0400

    Added validation for Twitter username

--- app/src/main/java/com/frostphyr/notiphy/AddTwitterActivity.java
+++ app/src/main/java/com/frostphyr/notiphy/AddTwitterActivity.java
@@ -1,7 +1,7 @@
 package com.frostphyr.notiphy;
 
 import android.os.Bundle;
-import android.widget.TextView;
+import android.widget.EditText;
 
 public class AddTwitterActivity extends AddEntryActivity {
 
@@ -15,8 +15,18 @@ public class AddTwitterActivity extends AddEntryActivity {
 
     @Override
     protected Entry createEntry() {
-        TextView usernameView = findViewById(R.id.username);
-        return new TwitterEntry(usernameView.getText().toString(), getMediaType(), getPhrases());
+        EditText usernameView = findViewById(R.id.username);
+        String username = usernameView.getText().toString().trim();
+        if (username.length() <= 0) {
+            usernameView.setError("Username required");
+        } else if (username.length() > 15) {
+            usernameView.setError("Username cannot be longer than 15 characters");
+        } else if (!username.matches("^[a-zA-Z0-9_]*$")) {
+            usernameView.setError("Username can only contain alphanumeric characters and underscores");
+        } else {
+            return new TwitterEntry(username, getMediaType(), getPhrases());
+        }
+        return null;
     }
 
 }
