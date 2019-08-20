44220ee041d7c6594328e0745d780594c655c78e
Frostphyr <omoshulol@gmail.com>
2019-08-19 21:13:15 -0400

    Added fix for notification redisplaying after it was cancelled

--- app/src/main/java/com/frostphyr/notiphy/notification/NotificationDispatcher.java
+++ app/src/main/java/com/frostphyr/notiphy/notification/NotificationDispatcher.java
@@ -8,6 +8,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Build;
+import android.util.Log;
 
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
@@ -26,13 +27,13 @@ public class NotificationDispatcher {
     private static final String CHANNEL_ID_PREFIX = "com.frostphyr.notiphy.channel.";
     private static final String CHANNEL_ID_LIST = CHANNEL_ID_PREFIX + "LIST";
 
-    private NsfwContent nsfwContent;
-    private boolean showMedia;
+    private static int id;
 
     private NotificationViews activeViews;
     private NotificationManagerCompat manager;
     private Context context;
-    private int id;
+    private NsfwContent nsfwContent;
+    private boolean showMedia;
 
     public NotificationDispatcher(Context context) {
         this.context = context;
@@ -41,35 +42,37 @@ public class NotificationDispatcher {
     }
 
     public static void dispatch(final Context context, NotificationViews views) {
-        String channelId = views instanceof MessageNotificationViews ?
-                CHANNEL_ID_PREFIX + ((MessageNotificationViews) views).getMessage().getType().getName() :
-                CHANNEL_ID_LIST;
-        views.setOnUpdateListener(new NotificationViews.OnUpdateListener() {
+        if (views.getId() == id || id == 0) {
+            String channelId = views instanceof MessageNotificationViews ?
+                    CHANNEL_ID_PREFIX + ((MessageNotificationViews) views).getMessage().getType().getName() :
+                    CHANNEL_ID_LIST;
+            views.setOnUpdateListener(new NotificationViews.OnUpdateListener() {
 
-            @Override
-            public void onUpdate(NotificationViews views) {
-                dispatch(context, views);
-            }
+                @Override
+                public void onUpdate(NotificationViews views) {
+                    dispatch(context, views);
+                }
 
-        });
-        NotificationManagerCompat.from(context).notify(views.getId(), new NotificationCompat.Builder(context, channelId)
-                .setOnlyAlertOnce(true)
-                .setAutoCancel(true)
-                .setSmallIcon(R.drawable.ic_notification_logo)
-                .setColor(ContextCompat.getColor(context, R.color.dark_text))
-                .setStyle(new NotificationCompat.DecoratedCustomViewStyle())
-                .setWhen(views.getWhen())
-                .setShowWhen(true)
-                .setCustomContentView(views.getSmallView())
-                .setCustomBigContentView(views.getBigView())
-                .setContentIntent(PendingIntent.getBroadcast(context, views.getId(),
-                        new Intent(context, LaunchNotificationBroadcastReceiver.class)
-                                .putExtra(EXTRA_ID, views.getId())
-                                .putExtra(LaunchNotificationBroadcastReceiver.EXTRA_LAUNCH_ACTIVITY_INTENT, views.getIntent()),
-                        PendingIntent.FLAG_UPDATE_CURRENT))
-                .setDeleteIntent(PendingIntent.getBroadcast(context, views.getId(),
-                        new Intent(ACTION_DISPOSE), PendingIntent.FLAG_UPDATE_CURRENT))
-                .build());
+            });
+            NotificationManagerCompat.from(context).notify(views.getId(), new NotificationCompat.Builder(context, channelId)
+                    .setOnlyAlertOnce(true)
+                    .setAutoCancel(true)
+                    .setSmallIcon(R.drawable.ic_notification_logo)
+                    .setColor(ContextCompat.getColor(context, R.color.dark_text))
+                    .setStyle(new NotificationCompat.DecoratedCustomViewStyle())
+                    .setWhen(views.getWhen())
+                    .setShowWhen(true)
+                    .setCustomContentView(views.getSmallView())
+                    .setCustomBigContentView(views.getBigView())
+                    .setContentIntent(PendingIntent.getBroadcast(context, views.getId(),
+                            new Intent(context, LaunchNotificationBroadcastReceiver.class)
+                                    .putExtra(EXTRA_ID, views.getId())
+                                    .putExtra(LaunchNotificationBroadcastReceiver.EXTRA_LAUNCH_ACTIVITY_INTENT, views.getIntent()),
+                            PendingIntent.FLAG_UPDATE_CURRENT))
+                    .setDeleteIntent(PendingIntent.getBroadcast(context, views.getId(),
+                            new Intent(ACTION_DISPOSE), PendingIntent.FLAG_UPDATE_CURRENT))
+                    .build());
+        }
     }
 
     public void dispatch(Message message) {
@@ -99,6 +102,7 @@ public class NotificationDispatcher {
         context.registerReceiver(disposeBroadcastReceiver, new IntentFilter(ACTION_DISPOSE));
 
         manager = NotificationManagerCompat.from(context);
+        manager.cancelAll();
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             NotificationManager notificationManager = context.getSystemService(NotificationManager.class);
