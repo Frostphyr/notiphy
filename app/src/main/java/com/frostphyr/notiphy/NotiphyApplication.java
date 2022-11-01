f3648d1fc9f363d2861861eccf0977fa710362ae
Frostphyr <omoshulol@gmail.com>
2022-11-01 13:52:47 -0400

    Updated dependencies, added Facebook client token, and added Play Integrity

--- app/src/main/java/com/frostphyr/notiphy/NotiphyApplication.java
+++ app/src/main/java/com/frostphyr/notiphy/NotiphyApplication.java
@@ -5,12 +5,20 @@ import androidx.multidex.MultiDexApplication;
 import com.frostphyr.notiphy.io.TokenUpdateWorker;
 import com.frostphyr.notiphy.settings.Setting;
 import com.google.android.gms.ads.MobileAds;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.appcheck.FirebaseAppCheck;
+import com.google.firebase.appcheck.playintegrity.PlayIntegrityAppCheckProviderFactory;
 
 public class NotiphyApplication extends MultiDexApplication {
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        FirebaseApp.initializeApp(getApplicationContext());
+        FirebaseAppCheck firebaseAppCheck = FirebaseAppCheck.getInstance();
+        firebaseAppCheck.installAppCheckProviderFactory(
+                PlayIntegrityAppCheckProviderFactory.getInstance());
 
         MobileAds.initialize(getApplicationContext());
         TokenUpdateWorker.schedule(getApplicationContext());
